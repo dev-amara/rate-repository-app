6a05b476277aeb6854d23a96f8be04d0d713ad31
Amara Bamba <amara.dianney@gmail.com>
2021-02-26 14:44:47 +0000

    Part10 ~ Exercise 10.11 - 10.16

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,48 +1,69 @@
-import React, {useCallback} from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
+import React, { useCallback, useContext } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
-import AppBarTab from "./AppBarTab";
-import theme from "../theme";
-import {Link, useHistory} from "react-router-native";
+import AppBarTab from './AppBarTab';
+import theme from '../theme';
+import { Link, useHistory } from 'react-router-native';
+import { useApolloClient } from '@apollo/client';
+import AuthStorageContext from '../contexts/AuthStorageContext';
+import useAuthorizedUser from '../hooks/useAuthorizedUser';
 
 const styles = StyleSheet.create({
     container: {
-        display: "flex",
+        display: 'flex',
         paddingTop: Constants.statusBarHeight,
         paddingBottom: Constants.statusBarHeight,
         backgroundColor: theme.colors.appBarBg,
     },
     scrollView: {
-        flexDirection: "row",
+        flexDirection: 'row',
         marginHorizontal: 20,
     },
 });
 
 const AppBar = () => {
+    const apolloClient = useApolloClient();
+    const authStorage = useContext(AuthStorageContext);
     const history = useHistory();
+    const { authorizedUser } = useAuthorizedUser();
 
     const routeTabPress = useCallback((path) => {
         history.push(path);
     }, []);
 
-    return <View style={styles.container}>
-        <ScrollView style={styles.scrollView} horizontal>
-            <Link
-                to="/"
-                title="Repositories"
-                path="/"
-                cb={routeTabPress}
-                component={AppBarTab}
-            />
-            <Link
-                to="/signin"
-                title="Sign in"
-                path="/signin"
-                cb={routeTabPress}
-                component={AppBarTab}
-            />
-        </ScrollView>
-    </View>
+    const signOut = useCallback(async (path) => {
+        await authStorage.removeAccessToken();
+        await apolloClient.resetStore();
+        history.push(path);
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <ScrollView style={styles.scrollView} horizontal>
+                <Link
+                    to="/"
+                    title="Repositories"
+                    path="/"
+                    cb={routeTabPress}
+                    component={AppBarTab}
+                />
+                <>
+                    {authorizedUser === null && (
+                        <Link
+                            to="/signin"
+                            title="Sign in"
+                            path="/signin"
+                            cb={routeTabPress}
+                            component={AppBarTab}
+                        />
+                    )}
+                    {authorizedUser && (
+                        <AppBarTab title="Sign out" path="/" cb={signOut} />
+                    )}
+                </>
+            </ScrollView>
+        </View>
+    );
 };
 
 export default AppBar;
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
-import { Formik } from "formik";
-import * as yup from "yup";
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
-import SignInForm from "./SignInForm";
+import useSignIn from '../hooks/useSignIn';
+import SignInForm from './SignInForm';
 
 const initialValues = {
-    username: "",
-    password: "",
+    username: '',
+    password: '',
 };
 
 const validationSchema = yup.object().shape({
-    username: yup.string().required("Username is required"),
-    password: yup.string().required("Password is required"),
+    username: yup.string().required('Username is required'),
+    password: yup.string().required('Password is required'),
 });
 
 const SignIn = () => {
+    const [signIn] = useSignIn();
+
     const onSubmit = async (values) => {
         const { username, password } = values;
-        console.log({ username, password })
+
+        try {
+            await signIn({ username, password });
+        } catch (e) {
+            console.log(e);
+        }
     };
 
-    return <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        validationSchema={validationSchema}
-    >
-        {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
-    </Formik>
+    return (
+        <Formik
+            initialValues={initialValues}
+            onSubmit={onSubmit}
+            validationSchema={validationSchema}
+        >
+            {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+        </Formik>
+    );
 };
 
 export default SignIn;
